736aa79fd61837482ccd3029fe91e2d678f54ae7
ardhikarn <ardhikarn@gmail.com>
2020-10-31 00:53:27 +0700

    Read Data Category

--- controllers/control_admin/control_category.js
+++ controllers/control_admin/control_category.js
@@ -1,13 +1,14 @@
-const Category = require("../../models/model_category");
+const modelCategory = require("../../models/model_category");
 
 module.exports = {
-  viewCategories: (req, res) => {
-    res.render("admin/categories/view_categories");
+  viewCategories: async (req, res) => {
+    const category = await modelCategory.find();
+    res.render("admin/categories/view_categories", { category });
   },
 
   addCategory: async (req, res) => {
     const { name } = req.body;
-    await Category.create({ name });
+    await modelCategory.create({ name });
     res.redirect("/admin/categories");
   },
 };
