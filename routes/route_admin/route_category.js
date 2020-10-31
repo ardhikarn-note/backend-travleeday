b52b2508cc33591f0fbb23a4fa8f1a0e1fc0533f
ardhikarn <ardhikarn@gmail.com>
2020-10-31 21:37:25 +0700

    Delete category

--- routes/route_admin/route_category.js
+++ routes/route_admin/route_category.js
@@ -3,10 +3,12 @@ const {
   viewCategories,
   addCategory,
   editCategory,
+  deleteCategory,
 } = require("../../controllers/control_admin/control_category");
 
 router.get("/", viewCategories);
 router.post("/", addCategory);
 router.put("/", editCategory);
+router.delete("/:id", deleteCategory);
 
 module.exports = router;
