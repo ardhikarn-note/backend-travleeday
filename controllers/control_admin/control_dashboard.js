ab250734e2557d0e87d9f4801ee5fbbe6bd5e37f
Ardhika Ryzha Nurmawan <62016621+ardhikarn@users.noreply.github.com>
2020-11-07 15:33:28 +0700

    dashboard admin

--- controllers/control_admin/control_dashboard.js
+++ controllers/control_admin/control_dashboard.js
@@ -1,11 +1,23 @@
+const modelMember = require("../../models/model_member");
+const modelItem = require("../../models/model_item");
+const modelBooking = require("../../models/model_booking");
+
 module.exports = {
-  viewDashboard: (req, res) => {
+  viewDashboard: async (req, res) => {
     try {
+      const member = await modelMember.find();
+      const booking = await modelBooking.find();
+      const item = await modelItem.find();
       const title = "Travleeday | Dashboard";
       res.render("admin/dashboard/view_dashboard", {
         title,
         user: req.session.user,
+        member,
+        booking,
+        item,
       });
-    } catch (error) {}
+    } catch (error) {
+      res.redirect("/admin/dashboard");
+    }
   },
 };
