c1e296c59d3479f0cb11eabd0258b1ee5121c461
Ardhika Ryzha Nurmawan <62016621+ardhikarn@users.noreply.github.com>
2020-11-07 13:42:57 +0700

    read booking data

--- controllers/control_admin/control_booking.js
+++ controllers/control_admin/control_booking.js
@@ -1,9 +1,20 @@
+const modelBooking = require("../../models/model_booking");
+const modelMember = require("../../models/model_member");
+const modelBank = require("../../models/model_booking");
+
 module.exports = {
-  viewBookings: (req, res) => {
-    const title = "Travleeday | Bookings";
-    res.render("admin/bookings/view_bookings", {
-      title,
-      user: req.session.user,
-    });
+  viewBookings: async (req, res) => {
+    try {
+      const booking = await modelBooking
+        .find()
+        .populate("memberId")
+        .populate("bankId");
+      const title = "Travleeday | Bookings";
+      res.render("admin/bookings/view_bookings", {
+        title,
+        user: req.session.user,
+        booking,
+      });
+    } catch (error) {}
   },
 };
